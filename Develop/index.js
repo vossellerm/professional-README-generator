e2056d760ceb9d76e976e3a4b0d433400b6a351d
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-22 19:20:31 -0700

    inquirer prompts

--- Develop/index.js
+++ Develop/index.js
@@ -11,21 +11,59 @@ const init = () => {
   inquirer
     .prompt([
       // description
-      {},
+      {
+        type: "input",
+        name: "description",
+        message: "Describe your project",
+      },
       // installation instructions
-      {},
+      {
+        type: "input",
+        name: "installation",
+        message: "What technology needs to be used or installed?",
+      },
       // usage info
-      {},
+      {
+        type: "input",
+        name: "usage",
+        message: "How can this project be used?",
+      },
       // contribution guidelines
-      {},
+      {
+        type: "input",
+        name: "contribution",
+        message: "What are your contribution guidelines?",
+      },
       // test instructions
-      {},
+      {
+        type: "input",
+        name: "test",
+        message: "How can your project be tested?",
+      },
       // license selection
-      {},
+      {
+        type: "list",
+        name: "license",
+        message: "What license did you use?",
+        choices: ["Apache", "Boost", "BSD 3", "BSD 2"],
+      },
       // GitHub username and link
-      {},
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
+      },
+      {
+        type: "input",
+        name: "githubLink",
+        message: "What is the link to you GitHub profile?",
+      },
       // email address
-      {},
+      {
+        type: "input",
+        name: "email",
+        message: "What email is the best place to reach you?",
+      },
     ])
     //   populate answers in generateMarkdown
     .then((answers) => {
