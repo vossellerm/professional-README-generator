45febf44adf208a7e4c2e63a6964b97e03b3cf86
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-22 18:10:40 -0700

    add starter code

---
+++ Develop/index.js
@@ -0,0 +1,5 @@
+// TODO: Include packages needed for this application
+
+// TODO: import the generateMarkdown function from utils/generateMarkdown.js
+
+// TODO: Write code to get user input, generate markdown, and save it to a file.
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,8 @@
+// TODO: Add code to generate markdown for README given a data object parameter containing the user input.
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
